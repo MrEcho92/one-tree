a410e8df404811b3260659755f0c73d09492428b
MrEcho92 <chigozie10@gmail.com>
2025-01-02 22:43:49 +0000

    more setup

--- frontend/src/core/theme/theme.ts
+++ frontend/src/core/theme/theme.ts
@@ -1,1 +1,14 @@
-export {};
+import { createTheme, ThemeOptions } from '@mui/material';
+
+const themeOptions: ThemeOptions = {
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+};
+
+export const theme = createTheme({ ...themeOptions });
