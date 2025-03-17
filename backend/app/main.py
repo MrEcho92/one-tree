6ac25e0dd4c6d7622f56d88c30e09210454bf9aa
MrEcho92 <chigozie10@gmail.com>
2025-03-17 11:26:30 +0000

    test

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
