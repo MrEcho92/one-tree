b2d89b6b369cccdf3e4b4c304464bb7710316ba2
MrEcho92 <chigozie10@gmail.com>
2025-04-01 22:48:11 +0100

    update landing ui

--- frontend/src/features/landing/components/FamilyTreeSection.tsx
+++ frontend/src/features/landing/components/FamilyTreeSection.tsx
@@ -8,6 +8,7 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import Grid from '@mui/material/Grid2';
+import Avatar from '@mui/material/Avatar';
 
 export function FamilyTreeSection() {
   const { palette, typography } = useTheme();
@@ -39,6 +40,7 @@ export function FamilyTreeSection() {
               md: typography.h1.fontWeight,
             },
             color: palette.primary.main,
+            py: 2,
           }}
         >
           Build your tree to connect your family&apos;s history.
@@ -50,7 +52,7 @@ export function FamilyTreeSection() {
               xs: typography.body1.fontSize,
               md: '18px',
             },
-            py: { xs: 2, md: 2 },
+            py: { xs: '0', md: 2 },
             lineHeight: '1.75',
           }}
         >
@@ -59,41 +61,29 @@ export function FamilyTreeSection() {
           where you are.
         </Typography>
       </Box>
-      <Grid container spacing={3} alignItems="center" justifyContent="center" py={4}>
-        <Grid size={{ xs: 12, sm: 12, md: 6 }}>
-          <Box
-            sx={{
-              p: 2,
-              display: 'flex',
-              flexDirection: 'column',
-              gap: 1,
-            }}
-          >
-            <Button>Start your family tree</Button>
-            <Button>View sample tree</Button>
-          </Box>
-        </Grid>
+      <Grid container alignItems="center" justifyContent="center" pb={5}>
         <Grid size={{ xs: 12, sm: 12, md: 6 }}>
           <Box
             sx={{
               display: 'flex',
               flexDirection: 'column',
-              gap: 2,
               alignItems: 'center',
+              backgroundColor: palette.background.default,
             }}
           >
-                        <Typography
+            <Typography
               sx={{
                 textAlign: 'center',
                 fontSize: {
                   xs: typography.h4.fontSize,
-                  md: typography.h2.fontSize,
+                  md: typography.h3.fontSize,
                 },
                 fontWeight: {
                   xs: typography.h3.fontWeight,
                   md: typography.h1.fontWeight,
                 },
                 color: palette.text.primary,
+                pt: { xs: 2, sm: 'none' },
               }}
             >
               {t('landing:instructions.gettingStarted')}
@@ -106,30 +96,57 @@ export function FamilyTreeSection() {
                 color: palette.text.secondary,
                 borderRadius: '8px',
                 p: 4,
-                backgroundColor: palette.background.paper,
               }}
             >
               <Box display="flex" alignItems="center" gap={2}>
-                <ChevronRightIcon color="primary" />
+                <Avatar sx={{ bgcolor: palette.secondary.main }}>1</Avatar>
                 <Typography variant="body1">
                   {t('landing:instructions.steps.stepOne')}
                 </Typography>
               </Box>
 
               <Box display="flex" alignItems="center" gap={2}>
-                <ChevronRightIcon color="primary" />
+                <Avatar sx={{ bgcolor: palette.secondary.main }}>2</Avatar>
                 <Typography variant="body1">
                   {t('landing:instructions.steps.stepTwo')}
                 </Typography>
               </Box>
 
               <Box display="flex" alignItems="center" gap={2}>
-                <ChevronRightIcon color="primary" />
+                <Avatar sx={{ bgcolor: palette.secondary.main }}>3</Avatar>
                 <Typography variant="body1">
                   {t('landing:instructions.steps.stepThree')}
                 </Typography>
               </Box>
+              <Box display="flex" alignItems="center" gap={2}>
+                <Avatar sx={{ bgcolor: palette.secondary.main }}>4</Avatar>
+                <Typography variant="body1">
+                  {t('landing:instructions.steps.stepFour')}
+                </Typography>
+              </Box>
             </Box>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              pt: 3,
+              gap: 2,
+            }}
+          >
+            <Button
+              variant="contained"
+              color="info"
+              disableElevation
+              onClick={() => navigate('/app')}
+            >
+              Start your family tree
+            </Button>
+            <Button variant="outlined" color="secondary">
+              View sample tree
+            </Button>
           </Box>
         </Grid>
       </Grid>
